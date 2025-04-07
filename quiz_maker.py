3dccb3dc44bc0054e2c59ddfed108b0c0aa24dbe
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-07 23:47:57 +0800

    the file maker was inside the loop, turns out i need to put it out. I also fixed the file maker cause it doesnt work, it needed "w" for it to write. Assignment complete : )

--- quiz_maker.py
+++ quiz_maker.py
@@ -4,36 +4,38 @@
 # assignment 9: this program will ask you to give questions and four different answers
 # with one of them being the correct answer and the other three being wrong. 
 
-# while loop
 # ask for the name of the quiz
-while True:
-    with open(input("Enter name of your quiz: " + ".txt")) as file:
+file_name = input("Enter the name of your quiz (without .txt): ") + ".txt"
+with open(file_name, "w") as file:
 
-    # input questions
-    # input answers
-    # input correct answer
-        question = input("Enter your question: ")
-        answer_a = input("Enter answer A: ")
-        answer_b = input("Enter answer B: ")
-        answer_c = input("Enter answer C: ")
-        answer_d = input("Enter answer D: ")
-        correct_answer = input("Enter the correct answer: ")
+    # while loop
+    while True:
 
-# make a file for the questions and answers to be stored in.
-        file.write("Question: " + question + "\n")
-        file.write("A: " + answer_a + "\n")
-        file.write("B: " + answer_b + "\n")
-        file.write("C: " + answer_c + "\n")
-        file.write("D: " + answer_d + "\n")
-        file.write("Correct Answer: " + correct_answer + "\n")
-        file.write("\n")
+        # input questions
+        # input answers
+        # input correct answer
+            question = input("Enter your question: ")
+            answer_a = input("Enter answer A: ")
+            answer_b = input("Enter answer B: ")
+            answer_c = input("Enter answer C: ")
+            answer_d = input("Enter answer D: ")
+            correct_answer = input("Enter the correct answer: ")
 
-# ask if done, if conditions yes or no, break or continue loop
-        done = input("do you want to input another question? (yes/no): ")
-        if done.lower() == "no":
-            break
-        elif done.lower() == "yes":
-            continue
-        else:
-            print("Invalid input. Please enter 'yes' or 'no'.")
-            break
+    # make a file for the questions and answers to be stored in.
+            file.write("Question: " + question + "\n")
+            file.write("A: " + answer_a + "\n")
+            file.write("B: " + answer_b + "\n")
+            file.write("C: " + answer_c + "\n")
+            file.write("D: " + answer_d + "\n")
+            file.write("Correct Answer: " + correct_answer + "\n")
+            file.write("\n")
+
+    # ask if done, if conditions yes or no, break or continue loop
+            done = input("do you want to input another question? (yes/no): ")
+            if done.lower() == "no":
+                break
+            elif done.lower() == "yes":
+                continue
+            else:
+                print("Invalid input. Please enter 'yes' or 'no'.")
+                break

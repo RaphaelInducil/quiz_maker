e2eee99eec449d7ff14b379e0aad4686a0953695
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-07 23:36:19 +0800

    made a loop of  the file that will record the qstns & answrs, also the ask inputs, and file writer.

--- quiz_maker.py
+++ quiz_maker.py
@@ -5,9 +5,28 @@
 # with one of them being the correct answer and the other three being wrong. 
 
 # while loop
-# make a file for the questions and answers to be stored in.
-    # input questions
-    # input answers
-    # input correct answer
-# ask if done, if conditions yes or no, break or continue loop
+# ask for the name of the quiz
 
+while True:
+    with open(input("Enter name of your quiz: " + ".txt" , "w")) as file:
+
+        # input questions
+        # input answers
+        # input correct answer
+        question = input("Enter your question: ")
+        answer_a = input("Enter answer A: ")
+        answer_b = input("Enter answer B: ")
+        answer_c = input("Enter answer C: ")
+        answer_d = input("Enter answer D: ")
+        correct_answer = input("Enter the correct answer: ")
+
+        file.write("Question: " + question + "\n")
+        file.write("A: " + answer_a + "\n")
+        file.write("B: " + answer_b + "\n")
+        file.write("C: " + answer_c + "\n")
+        file.write("D: " + answer_d + "\n")
+        file.write("Correct Answer: " + correct_answer + "\n")
+        file.write("\n")
+
+# make a file for the questions and answers to be stored in.
+# ask if done, if conditions yes or no, break or continue loop

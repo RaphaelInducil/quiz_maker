ed9d1bdbeae0583c44cba7cdf48c80454c03723d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-07 23:15:32 +0800

    pseudocode

---
+++ quiz_maker.py
@@ -0,0 +1,13 @@
+# raphael clouiee inducil
+# BSCpE 1-2 
+# 04-07-25
+# assignment 9: this program will ask you to give questions and four different answers
+# with one of them being the correct answer and the other three being wrong. 
+
+# while loop
+# make a file for the questions and answers to be stored in.
+    # input questions
+    # input answers
+    # input correct answer
+# ask if done, if conditions yes or no, break or continue loop
+

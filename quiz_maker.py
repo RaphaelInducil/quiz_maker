786ddf8288488fd3f2a95663138fc1b54c3f835b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-07 23:39:30 +0800

    added the "if" for repeating option, initial code done. not working atm

--- quiz_maker.py
+++ quiz_maker.py
@@ -6,13 +6,12 @@
 
 # while loop
 # ask for the name of the quiz
-
 while True:
-    with open(input("Enter name of your quiz: " + ".txt" , "w")) as file:
+    with open(input("Enter name of your quiz: " + ".txt")) as file:
 
-        # input questions
-        # input answers
-        # input correct answer
+    # input questions
+    # input answers
+    # input correct answer
         question = input("Enter your question: ")
         answer_a = input("Enter answer A: ")
         answer_b = input("Enter answer B: ")
@@ -20,6 +19,7 @@ while True:
         answer_d = input("Enter answer D: ")
         correct_answer = input("Enter the correct answer: ")
 
+# make a file for the questions and answers to be stored in.
         file.write("Question: " + question + "\n")
         file.write("A: " + answer_a + "\n")
         file.write("B: " + answer_b + "\n")
@@ -28,5 +28,12 @@ while True:
         file.write("Correct Answer: " + correct_answer + "\n")
         file.write("\n")
 
-# make a file for the questions and answers to be stored in.
 # ask if done, if conditions yes or no, break or continue loop
+        done = input("do you want to input another question? (yes/no): ")
+        if done.lower() == "no":
+            break
+        elif done.lower() == "yes":
+            continue
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+            break
